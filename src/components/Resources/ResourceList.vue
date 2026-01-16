eadf3e28f8bc630cd4feae40f0ded8b52d233860
stargazingsimple <stargazingsimple@gmail.com>
2026-01-16 17:33:21 +0200

    add resource-list component and styling components

---
+++ src/components/Resources/ResourceList.vue
@@ -0,0 +1,33 @@
+<script>
+import ResourceListItem from "@/components/Resources/ResourceListItem.vue";
+
+export default {
+  name: "ResourceList",
+  components: { ResourceListItem },
+  props: {
+    resources: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <ul>
+    <resource-list-item
+      v-for="resource in resources"
+      v-bind="resource"
+      :key="resource.id"
+    />
+  </ul>
+</template>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0 auto;
+  padding: 0;
+  max-width: 40rem;
+}
+</style>
