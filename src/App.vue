c2a5e71de7d60f22994e86646019c1d79e959e1b
stargazingsimple <stargazingsimple@gmail.com>
2026-01-16 16:07:34 +0200

    add resource-item component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,40 @@
 <script>
+import ResourceItem from "@/components/Resources/ResourceItem.vue";
+
 export default {
   name: "App",
+  components: { ResourceItem },
+  data() {
+    return {
+      resources: [
+        {
+          id: Math.random().toString(),
+          title: "Google",
+          description: "Google platform",
+          link: "https://google.com",
+        },
+        {
+          id: Math.random().toString(),
+          title: "Udemy",
+          description: "Udemy platform",
+          link: "https://udemy.com",
+        },
+        {
+          id: Math.random().toString(),
+          title: "Vue",
+          description: "Vue js platform",
+          link: "https://vuejs.org",
+        },
+      ],
+    };
+  },
 };
 </script>
 
 <template>
-  <h2>Hello world!!!</h2>
+  <resource-item
+    v-for="resource in resources"
+    v-bind="resource"
+    :key="resource.id"
+  />
 </template>
---
+++ src/components/Resources/ResourceItem.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  name: "ResourceItem",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <li>
+    <div>
+      <header>
+        <h3>{{ title }}</h3>
+        <button>Delete</button>
+      </header>
+    </div>
+    <p>{{ description }}</p>
+    <nav>
+      <a :href="link">View Resource</a>
+    </nav>
+  </li>
+</template>
